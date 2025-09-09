e1f5d1775712d5313bfce59b2af7b2e88004bd0f
Jake Low <hello@jakelow.com>
2025-09-08 23:12:43 -0700

    Exclute some place=* values from places layer

--- src/places.py
+++ src/places.py
@@ -1,19 +1,14 @@
 import sys
 import pyarrow
-import shapely.wkb
 
 from .geoparquet import GeoParquetWriter
 
 
 class PlacesWriter(GeoParquetWriter):
     """
-    The places layer contains point geometries representing named places
-    (cities, towns, neighborhoods, etc) from OSM - anything tagged place=*
-    that has a name.
-
-    Most place features are mapped as nodes, but some are mapped as areas
-    (typically neighborhoods, islands, etc). In these cases we include the
-    area's centroid in the output dataset.
+    The places layer contains point geometries representing named human
+    settlements (cities, towns, villages) and parts of settlements (boroughs,
+    qaurters, neighborhoods) from OSM.
     """
 
     COLUMNS = [
@@ -31,42 +26,30 @@ class PlacesWriter(GeoParquetWriter):
 
     FILTERS = {"place"}
 
+    PLACE_TYPES = {
+        # place=* values from OSM that represent human settlements
+        "city",
+        "town",
+        "village",
+        "hamlet",
+        "isolated_dwelling",
+        "farm",
+        "allotments"
+        # ...and parts of settlements:
+        "borough",
+        "suburb",
+        "quarter",
+        "neighborhood",
+        "city_block",
+    }
+
     def node(self, o):
-        if "place" not in o.tags or "name" not in o.tags:
+        if o.tags.get("place") not in self.PLACE_TYPES or "name" not in o.tags:
             return
 
         try:
-            self.append("node", o.id, self.columns(o.tags), self.wkbfactory.create_point(o))
-        except RuntimeError as e:
-            print(e, file=sys.stderr)
-
-    def area(self, o):
-        if "place" not in o.tags or "name" not in o.tags:
-            return
-
-        if not o.from_way() and "boundary" in o.tags:
-            # Administrativie boundary relations often have a 'label' member,
-            # which is itself a place=* node. To avoid creating multiple rows
-            # for a given place (city, town, etc), we skip boundary relations
-            # that are tagged place=*. It would be better to check if the
-            # relation actually has a member with the 'label' role, but Osmium
-            # doesn't let us access members on Area instances, so we're forced
-            # to just assume.
-            return
-
-        try:
-            # The "places" layer contains only POINT geometries; for areas
-            # tagged place=* we use the centroid.
-            polygon_wkb = self.wkbfactory.create_multipolygon(o)
-            polygon = shapely.wkb.loads(polygon_wkb, hex=True)
-            centroid = polygon.centroid
-            centroid_wkb = shapely.wkb.dumps(centroid).hex()
-
             self.append(
-                "way" if o.from_way() else "relation",
-                o.orig_id(),
-                self.columns(o.tags),
-                centroid_wkb,
+                "node", o.id, self.columns(o.tags), self.wkbfactory.create_point(o)
             )
         except RuntimeError as e:
             print(e, file=sys.stderr)
