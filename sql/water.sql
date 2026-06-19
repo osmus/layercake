beeba190618f2073b53841f7cf904c3a7d4d37b7
matt rice <ratmice@gmail.com>
2026-06-19 13:50:13 -0700

    Add some more tags

--- sql/water.sql
+++ sql/water.sql
@@ -19,7 +19,20 @@ COPY (
         tags['leisure'] IN ('slipway', 'marina') OR
         tags['portage'] IS NOT NULL OR
         tags['canoe'] IS NOT NULL OR
-        tags['mooring'] IS NOT NULL
+        tags['mooring'] IS NOT NULL OR
+        (tags['landuse'] = 'industrial' AND tags['industrial'] = 'port') OR
+        ( -- All deprecated in favor of tags['emergency'] = 'water_rescue'
+          tags['emergency'] IN ('lifeboat_station', 'marine_rescue') OR
+          tags['amenity'] = 'lifeboat_station'
+        ) OR
+        tags['emergency'] IN ( 'lifeguard', 'water_rescue', 'life_ring', 'throw_bag', 'rescue_buoy') OR
+        (
+          tags['emergency'] = 'assembly_point' AND (
+            tags['assembly_point:tsunami'] IS NOT NULL OR
+            tags['assembly_point:storm_surge'] IS NOT NULL
+          )
+        ) OR
+        tags['landuse'] IN ('basin', 'reservoir', 'harbour') -- All deprecated synonyms
       )
   )
   SELECT
@@ -45,7 +58,13 @@ COPY (
     tags['seasonal']                         AS seasonal,
     tags['water']                            AS water,
     tags['bridge']                           AS bridge,
+    tags['lifeguard']                        AS lifeguard,
+    tags['emergency']                        AS emergency,
+    tags['landuse']                          AS landuse,
+    tags['industrial']                       AS industrial,
+    tags['amenity']                          AS amenity,
     prefix_map('seamark:', tags)             AS seamark,
+    prefix_map('assembly_point:', tags)      AS assembly_point,
     split_multi(tags['name'])                AS name,
     prefix_map_split('name:', tags)          AS names,
     split_multi(tags['official_name'])       AS official_name,
