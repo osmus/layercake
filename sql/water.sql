273a60ab289e002a11f4ab80bcfa6cf1c206e978
matt rice <ratmice@gmail.com>
2026-07-29 22:12:19 -0700

    Remove some tags after review

--- sql/water.sql
+++ sql/water.sql
@@ -3,57 +3,45 @@ COPY (
     SELECT type, id, tags, geometry
     FROM '{{INPUT}}'
     WHERE kind = 'line'
-      AND tags['waterway'] IN ('river', 'stream', 'canal', 'ditch', 'drain', 'flowline', 'fairway', 'link',
-        -- Reason: Routing obstructions.
-        'dam', 'weir', 'waterfall'
-      )
+      AND tags['waterway'] IN ('river', 'stream', 'canal', 'ditch', 'drain', 'flowline', 'fairway', 'link')
   )
   SELECT
     type,
     id,
     tags['waterway']        AS waterway,
-    tags['name']            AS name,
+    split_multi(tags['name']) AS name,
+    prefix_map_split('name:', tags) AS names,
     tags['ref']             AS ref,
     tags['intermittent']    AS intermittent,
     tags['layer']           AS layer,
     tags['bridge']          AS bridge,
     tags['tunnel']          AS tunnel,
     tags['access']          AS access,
-    
-    -- waterway-relevant tags i've added or changed...   
-      -- Reason: general
+    -- General
       tags['usage']           AS usage,
-      tags['natural']         AS natural,
       split_multi(tags['seasonal']) AS seasonal,
       tags['tidal']           AS tidal,
-
-      -- Reason: dimensions.
+      -- Dimensions.
       tags['width']           AS width,
       tags['depth']           AS depth,
-      tags['depth:']          AS 'depth:',
-      tags['height']          AS height,    -- For waterfalls?
-
-      -- Reason: navigation direction and obstruction bypass,
-      prefix_map('oneway:', tags)    AS 'oneway:',
+      -- Navigation direction and obstruction bypass,
       tags['oneway']          AS oneway,
-      tags['canoe_pass']      AS canoe_pass,
       tags['lock']            AS lock,
-
+      tags['lock_name']       AS lock,
+      tags['lock_ref']        AS lock,
       -- Boat access
-      map_from_tag_list(tags, ['motorboat', 'ship', 'sailboat', 'boat', 'canoe']) as vessel_access,
-
+      tags['motorboat']	      AS motorboat,
+      tags['ship']            AS ship,
+      tags['sailboat']	      AS sailboat,
+      tags['boat']            AS boat,
+      tags['canoe']   	      AS canoe,
       -- Reason: Hazards,
       tags['open_water']      AS open_water,
-      prefix_map('whitewater:', tags) AS whitewater,
+      prefix_map('whitewater:', tags) AS whitewaters,
       tags['narrow']          AS narrow,
       tags['rapids']          AS rapids,
-      tags['rapids:']         AS rapids,
+      tags['rapids:name']     AS 'rapids:name',
       tags['hazard']          AS hazard,
-
-      -- Reason: Fish navigation?
-      tags['fish_pass']       AS fish_pass,   
-      -- Reason: hydrology
-      tags['order:strahler']  AS 'order:strahler',
     {
       xmin: ST_XMin(geometry)::FLOAT,
       ymin: ST_YMin(geometry)::FLOAT,
